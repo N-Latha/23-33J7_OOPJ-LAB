e236453ea1bd8e17265c5763d28355716a4f2086
N-Latha <nlatha270@gmail.com>
2025-01-30 22:07:05 +0530

    Add files via upload
    w11

---
+++ defaultvalues.java
@@ -0,0 +1,31 @@
+class defaultvalues
+{
+  static byte by;
+  static short s;
+  static int i;
+  static long l;
+  static float f;
+  static double d;
+  static boolean bl;
+  static char c;
+  public static void main(String[]args){
+  System.out.println("The default values of primitive datatypes");
+  System.out.println("The default values of byte:"+ by);
+  System.out.println("The default values of int:"+ i);
+  System.out.println("The default values of long:"+ l);
+  System.out.println("The default values of float:"+ f);
+  System.out.println("The default values of double:"+ d);
+  System.out.println("The default values of boolean:"+ bl);
+  System.out.println("The default values of char:"+ c);
+}
+ }
+  
+output:
+The default values of primitive datatypes
+The default values of byte:0
+The default values of int:0
+The default values of long:0
+The default values of float:0.0
+The default values of double:0.0
+The default values of boolean:false
+The default values of char:
